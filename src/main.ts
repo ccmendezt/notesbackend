4c88eec33b1033173852491a84cb237a1cfb1902
ccmendezt <ccmendezt@udistrital.edu.co>
2023-05-21 12:11:41 -0500

    Changes CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 5000;
+  app.enableCors({
+    origin: '*', // Reemplaza con el origen permitido para tu aplicación de React
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Especifica los métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Especifica las cabeceras permitidas
+  });
+
   await app.listen(PORT);
 }
 bootstrap();
